b26c7c41018aeb67bbd03c3e24f58362e15c2e53
Drew Bornt <andrew.bornt@gmail.com>
2025-02-26 20:41:22 -0600

    Gun follows target game object, which follows mouse position.

--- Assets/Scripts/Player/GunPivotPoint.cs
+++ Assets/Scripts/Player/GunPivotPoint.cs
@@ -1,29 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class GunPivotPoint : MonoBehaviour
 {
     [SerializeField] private GameObject target;
 
-    private Camera m_camera; 
-
-    void Start()
-    {
-        m_camera = Camera.main;
-    }
-
     void Update()
     {
-        /*Vector3 input = Input.mousePosition;
 
-        Vector3 mousePosition = m_camera.ScreenToWorldPoint(new Vector3(input.x, input.y, m_camera.transform.position.y));*/
-
-
-        /*Vector3 direction = (mousePosition - transform.position).normalized;*/
-
-        Vector3 direction = (target.transform.position - transform.position).normalized;
+        Vector2 direction = (target.transform.position - transform.position).normalized;
+        // Lets make this a vector2, we're in 2D space, no need for 3D vectors.
 
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
---
+++ Assets/TargetFollowMouse.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TargetFollowMouse : MonoBehaviour
+{
+    [SerializeField] private Camera m_Camera; // keeping the naming scheme that you used
+
+    private void Start()
+    {
+        m_Camera = Camera.main;
+        // There is only have the one camera in the scene and it's automatically the main camera.
+        // You have the right idea of caching the camera reference in Start() to avoid calling Camera.main every frame, like I do in my script, I think.
+    }
+
+    private void Update()
+    {
+        if (m_Camera != null)
+        {
+            transform.position = m_Camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        }
+    }
+}
